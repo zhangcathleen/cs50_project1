c748a54a547678e979a8c70853723ea6569c67f1
cat <zhangycathleen@gmail.com>
2020-06-09 19:12:25 -0500

    got books/isbn working, got the raw sql command working, fetches info from book_table and the goodreads api

--- application.py
+++ application.py
@@ -5,7 +5,11 @@ from goodreads import *
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.sql import text
 
+# export FLASK_APP=application.py
+# export DATABASE_URL = postgres://xhwfyyyajrayzc:98be0231bd360be4522c1e5afcec40a58d1ab3999c9f9457a9e1d21d182edd55@ec2-34-232-147-86.compute-1.amazonaws.com: 5432/db51ts8th98q5a
+# export FLASK_DEBUG=1
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -54,6 +58,12 @@ def display():
 def book():
 
     isbn = request.form["isbn_number"]
+    return books(isbn)
+
+
+@app.route("/books/<isbn>")
+def books(isbn):
+
     res = requests.get("https://www.goodreads.com/book/review_counts.json",
                        params={"key": "yGgwZLPLZpqS8IAGXcPA", "isbns": isbn})
     try:
@@ -62,26 +72,32 @@ def book():
         return render_template("error.html", message="No book with this name on gr", isbn=isbn)
 
     try:
-        book_table = Book.query.get(isbn)
-    except:
-        return render_template("error.html", message="No book with this name on table", isbn=isbn)
+        t = (isbn,)
+        # print(t)
+        cmd = "SELECT * FROM books WHERE isbn = :x "
+        # x = isbn
+        book_table = db.execute(text(cmd), {'x': isbn}).fetchall()
+        db.commit()
+    except Exception as e:
+        return render_template("error.html", message="No book with this isbn on table", isbn=isbn, e=e)
 
     info = {}
-    info[isbn] = isbn
     json = res.json()
     json_info = json['books'][0]
     info['isbn'] = isbn
     info['isbn13'] = json_info['isbn13']
-    info['ratings_count'] = json_info['ratings_count']
+    info['review_count'] = json_info['reviews_count']
     info['average_rating'] = json_info['average_rating']
-    info['title'] = book_table.title
-    info['author'] = book_table.author
-    info['year'] = book_table.year
+    # print(book_table)
+    # print(book_table.json())
+    info['title'] = book_table[0][1]
+    info['author'] = book_table[0][2]
+    info['year'] = book_table[0][3]
 
-    print(info)
+    # print(info)
     return render_template("book.html", info=info)
 
 
-@app.route("/form", methods=["POST"])
+@ app.route("/form", methods=["POST"])
 def form():
     return render_template("form.html")
